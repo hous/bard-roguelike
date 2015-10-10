278b7dd67a9bec3c05a07e8bde73aaee7408b0bf
Adam Housman <hous27@gmail.com>
2015-10-10 19:51:01 -0400

    Step 2 of the tutorial.

---
+++ README.md
@@ -0,0 +1,5 @@
+
+OSX libtcod setup: https://github.com/podiki/libtcod-mac
+Tutorial: http://www.roguebasin.com/index.php?title=Complete_Roguelike_Tutorial,_using_python
+
+@step2 in the tutorial, need to split things up in classes and not include libraries.
---
+++ main.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+import sys, os
+
+sys.path.append(os.path.abspath("./libtcod-mac/python"))
+sys.path.append(os.path.abspath("./classes"))
+
+
+import libtcodpy as libtcod
+import Object as O
+
+#actual size of the window
+SCREEN_WIDTH = 80
+SCREEN_HEIGHT = 50
+
+def handle_keys():
+    global player_x, player_y
+
+    key = libtcod.console_wait_for_keypress(True)
+
+    if key.vk == libtcod.KEY_ENTER and key.lalt:
+        #Alt+Enter: toggle fullscreen
+        libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
+
+    elif key.vk == libtcod.KEY_ESCAPE:
+        return True  #exit game
+
+    #movement keys
+    if libtcod.console_is_key_pressed(libtcod.KEY_UP):
+        player.move(0, -1)
+
+    elif libtcod.console_is_key_pressed(libtcod.KEY_DOWN):
+        player.move(0, 1)
+
+    elif libtcod.console_is_key_pressed(libtcod.KEY_LEFT):
+        player.move(-1, 0)
+
+    elif libtcod.console_is_key_pressed(libtcod.KEY_RIGHT):
+        player.move(1, 0)
+
+
+#############################################
+# Initialization & Main Loop
+#############################################
+
+libtcod.console_set_custom_font('libtcod-mac/data/fonts/arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
+libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, 'python/libtcod tutorial', False)
+con = libtcod.console_new(SCREEN_WIDTH, SCREEN_HEIGHT)
+
+player = O.Object(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, '@', libtcod.white)
+npc = O.Object(SCREEN_WIDTH/2 - 5, SCREEN_HEIGHT/2, '@', libtcod.yellow)
+objects = [npc, player]
+
+while not libtcod.console_is_window_closed():
+
+    #draw all objects in the list
+    for object in objects:
+        object.draw()
+
+    libtcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
+    libtcod.console_flush()
+
+    #erase all objects at their old locations, before they move
+    for object in objects:
+        object.clear()
+
+    #handle keys and exit game if needed
+    exit = handle_keys()
+    if exit:
+        break
